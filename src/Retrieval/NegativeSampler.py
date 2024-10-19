8bf2358ed0858daa6112cdbbfabac712e5987505
rasauq1122 <rasauq1122@gmail.com>
2024-10-19 21:14:24 +0900

    refactor: NegativeSampler가 num_negatives 변수를 method의 인자로 사용한다.

--- src/Retrieval/NegativeSampler.py
+++ src/Retrieval/NegativeSampler.py
@@ -1,16 +1,15 @@
 import numpy as np
 
 class NegativeSampler:
-    def __init__(self, num_negatives, corpus):
-        self.num_negatives = num_negatives
+    def __init__(self, corpus):
         self.corpus = np.array(corpus)
         
-    def offer(self, row):
+    def offer(self, row, num_negatives):
         context = row["context"]
         question = row["question"]
         
         while True:
-            neg_idxs = np.random.randint(len(self.corpus), size=self.num_negatives)
+            neg_idxs = np.random.randint(len(self.corpus), size=num_negatives)
             if not context in self.corpus[neg_idxs]:
                 p_neg = self.corpus[neg_idxs]
 
@@ -20,13 +19,13 @@ class NegativeSampler:
                     "negatives": p_neg
                 }
 
-    def offer_bulk(self, dataset):
+    def offer_bulk(self, dataset, num_negatives):
         questions = []
         contexts = []
         p_negs = []
         
         for row in dataset:
-            sample = self.offer(row)
+            sample = self.offer(row, num_negatives)
             questions.append(sample["question"])
             contexts.append(sample["context"])
             p_negs.append(sample["negatives"])
